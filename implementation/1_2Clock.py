8f7cf3ed8fcc0d645a2ce4cc9c62e111a800a8ae
NTK0203 <newthink0203@naver.com>
2023-07-12 13:48:56 +0000

    예제 1-2 최적화

--- implementation/1_2Clock.py
+++ implementation/1_2Clock.py
@@ -6,13 +6,6 @@ count=0
 for hour in range(N+1):
     for min in range(60):
         for sec in range(60):
-            if(hour%10==3):
+            if '3' in str(hour) + str(min) + str(sec): #문자열로 변환해서 조건문 최적화
                 count+=1
-                continue
-            elif(min%10==3 or min//10==3):
-                count+=1
-                continue
-            elif(sec%10==3 or sec//10==3):
-                count+=1
-                continue
 print(count)
