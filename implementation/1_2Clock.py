49aa8510ac750465ee63fb943d25ba64951d8528
NTK0203 <newthink0203@naver.com>
2023-07-12 13:43:29 +0000

    구현 예제 1-2 풀이

--- implementation/1_2Clock.py
+++ implementation/1_2Clock.py
@@ -2,5 +2,17 @@ N = int(input())
 hour = 0
 min = 0
 sec = 0
-while(hour!=N+1):
-    
+count=0
+for hour in range(N+1):
+    for min in range(60):
+        for sec in range(60):
+            if(hour%10==3):
+                count+=1
+                continue
+            elif(min%10==3 or min//10==3):
+                count+=1
+                continue
+            elif(sec%10==3 or sec//10==3):
+                count+=1
+                continue
+print(count)
