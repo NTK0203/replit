4f8ba94001b2186f7c52323c4682915547de15c4
NTK0203 <newthink0203@naver.com>
2023-07-28 14:49:02 +0000

    bfs 구현

---
+++ dfsbfs/bfs.py
@@ -0,0 +1,32 @@
+
+from collections import deque
+
+queue = deque()
+
+def bfs(graph, v, visited):
+    visited[v]=True
+    print(v,end=' ')
+    queue.append(v)
+    while(queue):
+        for i in range(len(graph[v])):
+            if not visited[graph[v][i]]:
+                queue.append(graph[v][i])
+                print(graph[v][i],end=' ')
+                visited[graph[v][i]]=True
+        v=queue.popleft()
+        
+    
+
+graph=[
+  [],
+  [2,3,8],
+  [1,7],
+  [1,4,5],
+  [3,5],
+  [3,4],
+  [7],
+  [2,6,8],
+  [1,7]
+]
+visited=[False]*9
+bfs(graph,1,visited)
